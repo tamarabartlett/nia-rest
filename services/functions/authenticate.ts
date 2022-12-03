c36ff0feed856cbff25dd4d3af8433d1cd8a05e3
Tamara Bartlett <tamjbart@gmail.com>
2022-12-02 18:12:06 -0800

    read env vars for the clientId and clienSecret for authenticate

--- services/functions/authenticate.ts
+++ services/functions/authenticate.ts
@@ -1,32 +1,35 @@
 import https from 'https';
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
-export const main: APIGatewayProxyHandlerV2 = async () => {
+export const main: APIGatewayProxyHandlerV2 = async (event) => {
+  const url = process.env.url
+  const clientId = process.env.clientId
+  const clientSecret = process.env.clientSecret
+  const appKey = process.env.appKey
+
+  console.log(event)
   try {
-    const result = await postRequest({
-      "clientID": process.env.CLIENT_ID,
-      "clientSecret": process.env.CLIENT_SECRET
-    });
+    const result = await postRequest(url, clientId, clientSecret, appKey);
 
     return {
       statusCode: 200,
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(result.value),
+      body: JSON.stringify(result),
     }
   } finally {
     console.log("Party on finally")
   }
 };
 
-function postRequest(body) {
+function postRequest(url: string , clientId: string , clientSecret: string , appKey: string) {
   const options = {
-    hostname: process.env.DRIVEWEALTH_URL,
+    hostname: url,
     path: '/back-office/auth/tokens',
     method: 'POST',
     port: 443, // 👈️ replace with 80 for HTTP requests
     headers: {
       'Content-Type': 'application/json',
-      'dw-client-app-key': process.env.APP_KEY,
+      'dw-client-app-key': appKey,
     },
   };
 
@@ -52,7 +55,10 @@ function postRequest(body) {
     });
 
     // 👇️ write the body to the Request object
-    req.write(JSON.stringify(body));
+    req.write(JSON.stringify({
+      "clientID": clientId,
+      "clientSecret": clientSecret
+    }));
     req.end();
   });
 }
